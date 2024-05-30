19fa0da7824b17f3f739bd121da4e08b669674a8
clementbo <clementbo50@gmail.com>
2024-05-30 22:14:10 +0200

    Création de l'objet Pokemon est mis en place d'une liste d'objet de pokemons avec leur informations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { POKEMONS } from './models/pokemons-list';
+import { Pokemon } from './models/pokemon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonsList: Pokemon[] = POKEMONS;
+  ngOnInit() {
+    console.table(this.pokemonsList);
+    this.selectPokemon(this.pokemonsList[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez choisi ${pokemon.name}`);
+  }
 }
---
+++ src/app/models/pokemon.ts
@@ -0,0 +1,12 @@
+﻿export class Pokemon {
+    constructor(public id: number,
+         public name: string,
+         public hp: number,
+         public cp: number,
+         public picture: string,
+         public types: string[],
+         public created: Date
+        ){}
+
+        
+}
