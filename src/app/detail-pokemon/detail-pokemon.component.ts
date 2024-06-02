aa8a1b9df490968db1d12a8a2e167a13dfb9e32c
clementbo <clementbo50@gmail.com>
2024-06-02 17:57:16 +0200

    Création de la logique pour recuperer le detail d'un pokemon
    Par la route /pokemons/:id

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../models/pokemons-list';
+import { Pokemon } from '../models/pokemon';
 @Component({
   selector: 'app-detail-pokemon',
   standalone: true,
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './detail-pokemon.component.html',
   styleUrl: './detail-pokemon.component.scss'
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent implements OnInit {
 
+  pokemonList!: Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+
+    if(pokemonId){
+      this.pokemon= this.pokemonList.find(pokemon => pokemon.id === +pokemonId);
+    }
+  }
 }
