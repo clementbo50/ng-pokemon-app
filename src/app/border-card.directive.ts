39a6bb10366f31e0d3a81c0db3872f1a15b82c8c
clementbo <clementbo50@gmail.com>
2024-06-02 15:31:39 +0200

    Appliquer les évenements utilisateurs dans la directive
    Prendre en compte les actions de l'utilisateur dans la directive

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]',
@@ -11,6 +11,17 @@ export class BorderCardDirective {
     this.setBorder('#f5f5f5');
   }
 
+  /* Evenements pour le hover sur les cartes */
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder('#4286f4');
+
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  }
+
+  /*methode pour changer la hauteur de la carte et lui appliquer une bordure*/
   setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
