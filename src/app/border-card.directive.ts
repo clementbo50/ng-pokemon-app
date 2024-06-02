f519c8d660c03bf325504034b6fa811e560ddc65
clementbo <clementbo50@gmail.com>
2024-06-02 16:17:03 +0200

    Amélioration du code en propriété
    remplaçage du code en dur en propriété

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -6,21 +6,26 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = 'grey';
+  private defaultColor: string = '#4286f4';
+  private defaultHeight: number = 180;
+
   constructor(private el:ElementRef ) { 
 
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
-  @Input('pkmnBorderCard') borderColor!: string;
+  @Input('pkmnBorderCard') borderColor!: string; //alias de la directive
+
 
   /* Evenements pour le hover sur les cartes */
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor || '#4286f4');
+    this.setBorder(this.borderColor || this.defaultColor);
 
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   /*methode pour changer la hauteur de la carte et lui appliquer une bordure*/
